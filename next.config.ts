0acea957cd7658c38c0116dce9d6aca4fa7fb04d
Kevin Tran <kevintr303@gmail.com>
2026-01-22 09:26:17 -0800

    Allow Cloudflare Insights script in CSP

--- next.config.ts
+++ next.config.ts
@@ -38,7 +38,7 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://static.cloudflareinsights.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: https:",
